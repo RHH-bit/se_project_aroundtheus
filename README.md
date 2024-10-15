3476adbedbb6c80edee31f1ae3a7e9268abe6b6a
Ralph Holewinski <holewinskiralph8@outlook.com>
2024-10-14 20:27:30 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+# Project 3: Around The U.S.
+
+This is the third project of the Foftware Engineering program at TripleTen. It was created using HTML, CSS, and JavaScript based on the design brief.
+
+## Project features
+
+- semantic HTML5
+- Flexbox
+- Positioning
+- Flat BEM file structure
+- Profile editer modal
+- JavaScript photo array
+
+## Plan on improving the project
+
+the next major update i think would be maybe adding a database.
---
+++ components/Card.js
@@ -0,0 +1,56 @@
+export default class Card {
+  constructor({ name, link }, cardSelector, handleImageClick) {
+    this._name = name;
+    this._link = link;
+    this._cardSelector = cardSelector;
+    this._handleImageClick = handleImageClick;
+  }
+
+  _setEventListeners() {
+    // ".card__like-button"
+    this._cardElement
+      .querySelector(".card__like-button")
+      .addEventListener("click", () => {
+        this._handleLikeIcon();
+      });
+
+    // ".card__delete-button"
+    this._cardElement
+      .querySelector(".card__delete-button")
+      .addEventListener("click", () => {
+        this._handleDeleteCard();
+      });
+
+    this._cardElement
+      .querySelector(".card__image")
+      .addEventListener("click", () => {
+        this._handleImageClick(this._name, this._link);
+      });
+  }
+
+  _handleLikeIcon() {
+    this._cardElement
+      .querySelector(".card__like-button")
+      .classList.toggle("card__like-button_active");
+  }
+
+  _handleDeleteCard() {
+    this._cardElement.remove();
+    this._cardElement = null;
+  }
+
+  getView() {
+    this._cardElement = document
+      .querySelector(this._cardSelector)
+      .content.querySelector(".card")
+      .cloneNode(true);
+
+    this._cardElement.querySelector(".card__title").textContent = this._name;
+    this._cardElement.querySelector(".card__image").src = this._link;
+    this._cardElement.querySelector(".card__image").alt = this._name;
+
+    this._setEventListeners();
+
+    return this._cardElement;
+  }
+}
